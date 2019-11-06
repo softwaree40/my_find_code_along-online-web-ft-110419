6f1196370263920af5da548a7a1a89bff564215d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 15:13:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,8 @@ def my_find(collection)
    correct_element_to_block = []
    i = 0 
    while i < collection.size
-    
+    correct_element_to_block << yield(collection[i])
     i+=1
   end
-    correct_element_to_block << yield(1)
+    
 end
