8b9dd78e334fa77eafdf31c1d516a1ee52a8e12b
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 15:27:01 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,20 +1,12 @@
 require 'pry'
 
-#def my_find(collection)
-   #correct_element_to_block = []
-   #i = 0 
-   #while i < collection.size
-   # correct_element_to_block << yield(collection[i])
-   # i+=1
-  #end
-   
-#end
 def my_find(collection)
-  new_array = []
-  i = 0
-  while i < 100
-    new_array << yield(1)
-      i = i + 1
+   correct_element_to_block = []
+   i = 0 
+   while i < collection.size
+    correct_element_to_block << yield(collection[i])
+    i+=1
+    binding.pry
   end
-  return new_array
+   
 end
