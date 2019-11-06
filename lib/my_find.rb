06076436691c3b3b465df6e229bbf2b41e47d2a1
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 15:07:49 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+   correct_element_to_block = []
+   i = 0 
+   while i < collection.size
+     correct_element_to_block << yield(collection[i])
+    i+=1
+  end
+   correct_element_to_block
 end
